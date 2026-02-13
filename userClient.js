0e01094b872b6a520bea38ad1b563a5fafa14131
Arijit Mahato <98653181+Ari0710@users.noreply.github.com>
2026-02-13 13:00:45 +0530

    Update userClient.js

--- userClient.js
+++ userClient.js
@@ -1,29 +1,19 @@
 const { TelegramClient } = require("telegram");
 const { StringSession } = require("telegram/sessions");
-const input = require("input");
-const fs = require("fs");
 const env = require("./env");
 
-const session = fs.existsSync(env.sessionFile)
-  ? fs.readFileSync(env.sessionFile, "utf8")
-  : "";
+const stringSession = new StringSession(process.env.SESSION_STRING);
 
 const client = new TelegramClient(
-  new StringSession(session),
+  stringSession,
   env.apiId,
   env.apiHash,
   { connectionRetries: 5 }
 );
 
 async function startUserClient() {
-  await client.start({
-    phoneNumber: async () => input.text("Phone: "),
-    phoneCode: async () => input.text("Code: "),
-    password: async () => input.text("2FA password (if any): "),
-  });
-
-  fs.writeFileSync(env.sessionFile, client.session.save());
-  console.log("✅ User client logged in");
+  await client.start();
+  console.log("✅ User client started");
 }
 
 module.exports = { client, startUserClient };
